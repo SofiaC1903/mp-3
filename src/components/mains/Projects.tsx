8e426d67cacffb6c6d76fc18ebebc71e11a7a066
Sofia Ford <scfordw@bu.edu>
2025-10-21 13:32:51 -0400

    Fixing typo in Project.tsx

--- src/components/mains/Projects.tsx
+++ src/components/mains/Projects.tsx
@@ -11,7 +11,7 @@ export default function Projects(){
 
             <StyledTitle>Projects:</StyledTitle>
             <br/>
-            <Calculator></Calculator>
+            <Calculator/>
 
         </StyledMain>
     )
